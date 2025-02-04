2890047f2469c5b074f20ea5f062ed43d740d8d9
Vitaly Zorin <addefan@mail.ru>
2025-02-05 02:06:33 +0300

    feat(terraform): add server configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = pathexpand(sa_key_file_path)
+  service_account_key_file = pathexpand(var.sa_key_file_path)
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
---
+++ terraform/server.tf
@@ -0,0 +1,46 @@
+resource "yandex_compute_instance" "server" {
+  name        = "server"
+  platform_id = "standard-v3"
+  hostname    = "ubuntu"
+
+  resources {
+    core_fraction = 20
+    cores         = 2
+    memory        = 1
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot_disk.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/vvot.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "server-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "server-subnet"
+  zone           = var.zone
+  v4_cidr_blocks = ["192.168.0.0/24"]
+  network_id     = yandex_vpc_network.network.id
+}
+
+data "yandex_compute_image" "ubuntu_image" {
+  family = "ubuntu-2404-lts-oslogin"
+}
+
+resource "yandex_compute_disk" "boot_disk" {
+  name     = "server-boot-disk"
+  type     = "network-ssd"
+  image_id = data.yandex_compute_image.ubuntu_image.id
+  size     = 10
+}
