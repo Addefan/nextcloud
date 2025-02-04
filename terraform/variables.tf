51332841d279727101e13dea7a323601321df7c9
Vitaly Zorin <addefan@mail.ru>
2025-02-05 02:03:14 +0300

    feat(terraform): add variables

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "cloud_id" {
+  type        = string
+  description = "Идентификатор облака по умолчанию"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Идентификатор каталога по умолчанию"
+}
+
+variable "zone" {
+  type        = string
+  description = "Зона доступности по умолчанию"
+  default     = "ru-central1-d"
+}
+
+variable "sa_key_file_path" {
+  type        = string
+  description = "Путь к ключу сервисного аккаунта с ролью admin"
+  default     = "~/.yc-keys/key.json"
+}
